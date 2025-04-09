fad1e5cc433c415c6cc4e565f2805e901e88c273
Mansi Kabariya <mansi@revatics.co.uk>
2025-04-09 17:27:33 +0530

    clone for practice

--- lib/Hello_screen.dart
+++ lib/Hello_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 
-
-
-
 class HelloScreen extends StatelessWidget {
   const HelloScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(child: Text('Helloo world!'),),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HelloScreen(),
+      home: const HelloScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
