e3ad38f9e66dce67d637a87612d4ab8e3ecb3ffb
Mansi <mansi@revatics.co.uk>
2025-04-09 16:15:27 +0530

    new project start initial commit

---
+++ lib/Hello_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class HelloScreen extends StatelessWidget {
+  const HelloScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Helloo world!'),),
+    );
+  }
+}
